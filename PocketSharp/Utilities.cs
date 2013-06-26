678a8dfd44faa1b2684ba746cfdd8802d690ebb6
ceee <cee@live.at>
2013-06-27 00:28:10 +0200

    documentation for Utilities;

--- PocketSharp/Utilities.cs
+++ PocketSharp/Utilities.cs
@@ -6,8 +6,16 @@ using System.Text;
 
 namespace PocketSharp
 {
+  /// <summary>
+  /// General utilities
+  /// </summary>
   internal class Utilities
   {
+    /// <summary>
+    /// converts DateTime to an UNIX timestamp
+    /// </summary>
+    /// <param name="dateTime">The date.</param>
+    /// <returns>UNIX timestamp</returns>
     public static int? GetUnixTimestamp(DateTime? dateTime)
     {
       if(dateTime == null) return null;
@@ -16,6 +24,13 @@ namespace PocketSharp
     }
 
 
+    /// <summary>
+    /// Creates a Parameter object.
+    /// </summary>
+    /// <param name="name">The name.</param>
+    /// <param name="value">The value.</param>
+    /// <param name="type">The type.</param>
+    /// <returns></returns>
     public static Parameter CreateParam(string name, object value, ParameterType type = ParameterType.GetOrPost)
     {
       return new Parameter()
