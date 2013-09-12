25321ee107a9357932edb8f11aa67a875bc09d6d
ceee <cee@live.at>
2013-09-12 11:40:25 +0200

    update tests

--- PocketSharp.Tests/UnitTest1.cs
+++ PocketSharp.Tests/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
 
 namespace PocketSharp.Tests
 {
@@ -7,8 +8,19 @@ namespace PocketSharp.Tests
   public class UnitTest1
   {
     [TestMethod]
-    public void TestMethod1()
+    public async Task TestMethod1()
     {
+      var tcs = new TaskCompletionSource<string>();
+
+      PocketClient client = new PocketClient(
+        consumerKey: "15396-f6f92101d72c8e270a6c9bb3",
+        callbackUri: "http://frontendplay.com",
+        accessCode: "206d5286-230d-b3e2-48c6-fabf89"
+      );
+
+      var items = await client.Retrieve(RetrieveFilter.Archive);
+
+      Assert.AreEqual(10, items.Capacity);
     }
   }
 }
