813b1ab5b00f7fb1682b1fb01b8effd34ed726f2
ceee <cee@live.at>
2013-09-15 15:19:33 +0200

    update assembly info; create new nuspec file

--- PocketSharp/Properties/AssemblyInfo.cs
+++ PocketSharp/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PocketSharp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("PocketSharp is a .NET class library, that integrates the Pocket API v3")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("cee")]
 [assembly: AssemblyProduct("PocketSharp")]
@@ -25,5 +25,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.0")]
-[assembly: AssemblyFileVersion("0.4.0")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
