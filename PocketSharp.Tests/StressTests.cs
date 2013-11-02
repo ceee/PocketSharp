088f6a4e0eaf5ed78b48f5cfbe17732f22646a85
Tobias Klika <cee@live.at>
2013-11-02 10:32:38 +0100

    add stress test wrapper and URL csv's

---
+++ PocketSharp.Tests/StressTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Xunit;
+using PocketSharp.Models;
+
+namespace PocketSharp.Tests
+{
+  public class StressTests : TestsBase
+  {
+    public StressTests() : base() 
+    { 
+      // !! please don't misuse this account !!
+      client = new PocketClient(
+        consumerKey: "15396-f6f92101d72c8e270a6c9bb3",
+        callbackUri: "http://frontendplay.com",
+        accessCode: "80acf6c5-c198-03c0-b94c-e74402"
+      );
+    }
+
+
+    [Fact]
+    public void Are100ItemsRetrievedProperly()
+    {
+      
+    }
+
+    [Fact]
+    public void Are1000ItemsRetrievedProperly()
+    {
+
+    }
+
+    [Fact]
+    public void Are10000ItemsRetrievedProperly()
+    {
+
+    }
+
+    [Fact]
+    public void Are0ItemsRetrievedProperly()
+    {
+      
+    }
+
+    [Fact]
+    public void IsSearchedSuccessfullyOn10000Items()
+    {
+      
+    }
+  }
+}
