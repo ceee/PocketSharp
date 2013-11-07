6b3819b87b0fb4600b6e327b22c8023d6d17f6a7
Tobias Klika <cee@live.at>
2013-11-08 00:55:44 +0100

    require Uri to be absolute in Add method

--- PocketSharp/Components/Add.cs
+++ PocketSharp/Components/Add.cs
@@ -16,10 +16,18 @@ namespace PocketSharp
     /// <param name="tags">A comma-separated list of tags to apply to the item</param>
     /// <param name="title">This can be included for cases where an item does not have a title, which is typical for image or PDF URLs. If Pocket detects a title from the content of the page, this parameter will be ignored.</param>
     /// <param name="tweetID">If you are adding Pocket support to a Twitter client, please send along a reference to the tweet status id. This allows Pocket to show the original tweet alongside the article.</param>
-    /// <returns>A simple representation of the saved item which doesn't contain all data (is only returned by calling the Retrieve method)</returns>
+    /// <returns>
+    /// A simple representation of the saved item which doesn't contain all data (is only returned by calling the Retrieve method)
+    /// </returns>
+    /// <exception cref="System.FormatException">(1) Uri should be absolute.</exception>
     /// <exception cref="PocketException"></exception>
     public async Task<PocketItem> Add(Uri uri, string[] tags = null, string title = null, string tweetID = null)
     {
+      if (!uri.IsAbsoluteUri)
+      {
+        throw new FormatException("(1) Uri should be absolute.");
+      }
+
       AddParameters parameters = new AddParameters()
       {
         Uri = uri,
