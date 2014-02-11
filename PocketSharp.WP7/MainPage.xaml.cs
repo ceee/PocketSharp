07357b9327476d1dea66b3fbe59f5d18317103c9
Tobias Klika <cee@live.at>
2014-02-11 21:55:22 +0100

    add wp7 project

---
+++ PocketSharp.WP7/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Phone.Controls;
+using System.Windows;
+
+namespace PocketSharp.WP7
+{
+  public partial class MainPage : PhoneApplicationPage
+  {
+    // Constructor
+    public MainPage()
+    {
+      InitializeComponent();
+
+      // Set the data context of the LongListSelector control to the sample data
+      DataContext = App.ViewModel;
+    }
+
+    private async void Button_Click(object sender, RoutedEventArgs e)
+    {
+      await App.ViewModel.LoadData();
+    }
+  }
+}
