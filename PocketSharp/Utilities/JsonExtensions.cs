f62820e069fb781cd0fe2285fcf6b6a804656a0c
Tobias Klika <cee@live.at>
2014-07-21 16:37:30 +0200

    convert all datetimes to UTC+0

--- PocketSharp/Utilities/JsonExtensions.cs
+++ PocketSharp/Utilities/JsonExtensions.cs
@@ -32,8 +32,9 @@ namespace PocketSharp
   {
     public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
     {
-      DateTime epoc = new DateTime(1970, 1, 1);
-      var delta = (DateTime)value - epoc;
+      DateTime date = (DateTime)value;
+      DateTime epoc = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+      var delta = date - epoc;
 
       writer.WriteValue((long)delta.TotalSeconds);
     }
